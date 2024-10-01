470eef267d30cad277002c10a306728d9309ae78
Gian F Maciel <gian.f.maciel@gmail.com>
2024-09-30 21:14:39 -0300

    commit exercicios

---
+++ exercicio1.js
@@ -0,0 +1,24 @@
+let numerotres =3
+let numero4 = 4
+let numero5 = 5
+let numero234 = 234
+let numeromenos1 = -1
+let resultado
+
+resultado = numerotres + numero4
+
+console.log(resultado)
+
+resultado = numerotres * numero5
+resultado = resultado / 2
+
+console.log(resultado)
+
+resultado = numero4 - numero5
+resultado = resultado * numeromenos1
+
+console.log(resultado)
+
+resultado = numero234 % numero5
+
+console.log(resultado)
---
+++ exercicio2.js
@@ -0,0 +1,19 @@
+const primeiro = 5
+const segundo = 10
+let resultado1
+
+resultado1 = primeiro === segundo
+
+console.log(resultado1)
+
+resultado1 = primeiro !== segundo
+
+console.log(resultado1)
+
+resultado1 = primeiro > segundo
+
+console.log(resultado1)
+
+resultado1 = primeiro < segundo
+
+console.log(resultado1)
