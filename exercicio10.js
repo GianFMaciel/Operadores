2402778890355cbeca067da0efa5f8e296e14231
Gian F Maciel <gian.f.maciel@gmail.com>
2024-10-01 22:01:31 -0300

    commit exercicios

--- exercicio10.js
+++ exercicio10.js
@@ -2,4 +2,4 @@ let numeropar = prompt("insira um numero Par")
 
 let resto = Number(numeropar) % 2
 
-console.log(resto) // o resultado é sempre zero
+console.log(resto) // o resultado é sempre zero, se o usuario colocar um numero impar ele volta resultado 1 dependendo do numero impar colocado
---
+++ exercicio11.js
@@ -0,0 +1,9 @@
+let idadeemanos = prompt("Qual sua idade em anos de vida?")
+
+let anos = Number(idadeemanos)
+
+let meses = anos / 12
+
+let dias = meses / 365
+
+let horas = 
