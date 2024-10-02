93f30e49312dbec9cc68b27716f839f5a31604c7
Gian F Maciel <gian.f.maciel@gmail.com>
2024-10-02 19:14:26 -0300

    commit

--- exercicio11.js
+++ exercicio11.js
@@ -2,8 +2,13 @@ let idadeemanos = prompt("Qual sua idade em anos de vida?")
 
 let anos = Number(idadeemanos)
 
-let meses = anos / 12
+let meses = anos * 12
 
-let dias = meses / 365
+let dias = meses * 365.25
 
-let horas = 
+let horas = meses * 24
+
+console.log(anos)
+console.log(meses)
+console.log(dias)
+console.log(horas)
