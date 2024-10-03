a49ef5ec26117a4cde28e4c1148c1fde6af13e9a
Gian F Maciel <gian.f.maciel@gmail.com>
2024-10-02 21:50:37 -0300

    commit

--- exercicio12.js
+++ exercicio12.js
@@ -1,1 +1,22 @@
-let numero1 = prompt("Digite o primeiro numero")
+let numero1 = prompt("Digite o primeiro número")
+let numero2 = prompt("Digite o segundo número")
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Faça um programa que pergunte ao usuário dois números. Em seguida, faça as operações e imprima no console as seguintes mensagens seguidas pelo true ou false:
+O primeiro numero é maior que segundo? true
+O primeiro numero é igual ao segundo? false
+O primeiro numero é divisível pelo segundo? true
+O segundo numero é divisível pelo primeiro? true
+
+obs: O true ou false vai depender dos números inseridos e do resultado das operações.*/
